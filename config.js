8139bc9ddeb809c9d1d14ec766a1790673cd407f
nghuubang <93261915+nghuubang@users.noreply.github.com>
2021-11-14 15:19:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+	introTitle : 'Em à!' ,
+	introDesc : Thường lệ vốn trái đất
+	Mà sao em cứ đi nhầm đường
+	Lạc vào tim anh lẻ loi
+	Đằng sau chữ yêu đây là thương` ,
+	btnIntro : 'hihi' ,
+	title : 'Phải chăng em yêu ngay từ cái nhìn đầu tiên ,
+	desc : 'Phải chăng em đã nói ngay từ khi thấy nụ cười xuất hiện' ,
+	btnCó : ' Cứ là thích anh <33' ,
+	btnNo : 'Không, Anh trai à: 3' ,
+	question : 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3' ,
+	btnReply : 'Gửi cho anh <3' ,
+	reply : 'Yêu thì yêu mà không yêu thì yêu <33333333' ,
+	lộn xộn : 'Anh biết mà Yêu em nhiều ,
+	messDesc : 'Lamf ngiuuu anhh nhé<3.' ,
+	btn Chấp nhận : 'Okiiiii lun <3' ,
+	messLink : 'https://www.facebook.com/messages/t/100045119106201' // liên kết lộn xộn của các bạn. VD: https://m.me/nam.nodemy
+	}
