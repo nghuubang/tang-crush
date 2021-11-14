1c9734e9e1125c12e2787c8a8b8ca7a6cbf1e102
nghuubang <93261915+nghuubang@users.noreply.github.com>
2021-11-14 15:26:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 const CONFIG = {
 	introTitle : 'Em à!' ,
-	introDesc : Thường lệ vốn trái đất
-	Mà sao em cứ đi nhầm đường
-	Lạc vào tim anh lẻ loi
-	Đằng sau chữ yêu đây là thương` ,
-	btnIntro : 'hihi' ,
+	introDesc : Tại sao em cứ 
+	Loanh quanh
+	Trong tâm trí
+	Của Anh vậy` ,
+	btnIntro : 'hihi iuuu<3' ,
 	title : 'Phải chăng em yêu ngay từ cái nhìn đầu tiên ,
 	desc : 'Phải chăng em đã nói ngay từ khi thấy nụ cười xuất hiện' ,
 	btnCó : ' Cứ là thích anh <33' ,
 	btnNo : 'Không, Anh trai à: 3' ,
 	question : 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3' ,
 	btnReply : 'Gửi cho anh <3' ,
-	reply : 'Yêu thì yêu mà không yêu thì yêu <33333333' ,
+	reply : 'Em cũng không biết em yêu anh từ bao giờ <33333333' ,
 	lộn xộn : 'Anh biết mà Yêu em nhiều ,
-	messDesc : 'Lamf ngiuuu anhh nhé<3.' ,
+	messDesc : 'Làm ngiuuu anhh nhé<3.' ,
 	btn Chấp nhận : 'Okiiiii lun <3' ,
 	messLink : 'https://www.facebook.com/messages/t/100045119106201' // liên kết lộn xộn của các bạn. VD: https://m.me/nam.nodemy
 	}
+
